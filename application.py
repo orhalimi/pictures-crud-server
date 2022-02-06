c93efb73e736aa0e50ad5ef8ec9da1111471c3cf
orhalimi <or50002001@gmail.com>
2022-02-06 18:02:43 +0200

    Install sql alchemy

--- application.py
+++ application.py
@@ -1,20 +1,23 @@
 import os
 from flask import Flask, request
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 UPLOAD_FOLDER = 'static/upload'
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+engine = create_engine("mysql+pymysql://root:ka*dj#D23Aff2@localhost:3307/prod") #TODO: put in variable
+Session = sessionmaker(bind=engine)
 
 
 @app.route("/images", methods=["POST"])
 def get_images():
     files = request.files.getlist('images[]')
-    print(files)
-    for file in files:
-        file.save(os.path.join(app.config["UPLOAD_FOLDER"], file.filename))
+
     return None
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # app.run(debug=True)
+    pass
